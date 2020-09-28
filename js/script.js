b2a65af4d093560c3288754fe0a426e3c15e4fc2
Alessandro <alessandro.boscato@gmail.com>
2020-09-28 15:46:18 +0200

    Inseriti 4 metodi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 // Todolist con chiamate CRUD all'API  http://157.230.17.132:3007/todos
+
+$(document).ready(function(){
+
+  // CREATE
+
+  // $.ajax({
+  //   "url": "http://157.230.17.132:3007/todos",
+  //   "method": "POST",
+  //   "success": function(data){
+  //
+  //   },
+  //   "error": function(){
+  //     alert();
+  //   }
+  // });
+
+  // READ
+
+  // $.ajax({
+  //   "url": "http://157.230.17.132:3007/todos",
+  //   "method": "GET",
+  //   "success": function(data){
+  //
+  //   },
+  //   "error": function(){
+  //     alert();
+  //   }
+  // });
+
+  // UPDATE
+
+  // $.ajax({
+  //   "url": "http://157.230.17.132:3007/todos",
+  //   "method": "PATCH",
+  //   "success": function(data){
+  //
+  //   },
+  //   "error": function(){
+  //     alert();
+  //   }
+  // });
+
+  // DELETE
+  // 
+  // $.ajax({
+  //   "url": "http://157.230.17.132:3007/todos",
+  //   "method": "DELETE",
+  //   "success": function(data){
+  //
+  //   },
+  //   "error": function(){
+  //     alert();
+  //   }
+  // });
+
+});
