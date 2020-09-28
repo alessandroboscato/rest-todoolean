c4518425fe2aeea33541fd133830030c79b7d212
Alessandro <alessandro.boscato@gmail.com>
2020-09-28 21:47:41 +0200

    Metodo update

--- js/script.js
+++ js/script.js
@@ -42,30 +42,64 @@ $(document).ready(function(){
 
   $("#to-do-list").on("click", ".fa-edit", function(){
     $(this).next(".update").toggle();
-    $("#send-modified-data").click(function(){
-      var inputUser = $(this).siblings("#modify-data").val();
-      if (inputUser != "") {
-        var idSelected= $(this).parents("li").attr("data-attribute");
-
-        // UPDATE
-
-        $.ajax({
-          "url": "http://157.230.17.132:3007/todos/" + idSelected,
-          "method": "PATCH",
-          "data": {
-            "text": inputUser
-          },
-          "success": function(data){
-            alert("Hai modificato la voce");
-
-          },
-          "error": function(){
-            alert();
-          }
-        });
-      }
     })
-  })
+
+  $(document).on("click", "#send-modified-data", function(){
+    var todoId = $(this).parents("li.modify").attr("data-attribute");
+    var modifyListItem = $(this).siblings("#modify-data").val();
+    if (modifyListItem != "") {
+      $.ajax({
+        // UPDATE
+        "url": "http://157.230.17.132:3007/todos/" + todoId,
+        "method": "PATCH",
+        "data": {
+          "text": modifyListItem
+        },
+        "success": function(){
+          alert("Hai modificato con successo la voce della lista");
+          $("#to-do-list").html("");
+          createList();
+        },
+        "error": function(){
+          alert("Qualcosa è andato storto");
+        }
+      })
+    }
+
+  });
+
+
+  // ;
+  // });
+
+    // $("#send-modified-data").click(function(){
+    //   var inputUser = $(this).siblings("#modify-data").val();
+    //   if (inputUser != "") {
+    //     var todoId = $(this);
+    //     // var todoId= $(this).parents("li").data("id");
+    //     console.log(todoId);
+    //
+    //     // UPDATE
+
+        // $.ajax({
+        //   "url": "http://157.230.17.132:3007/todos/" + todoId,
+        //   "method": "PATCH",
+        //   "data": {
+        //     "text": inputUser
+        //   },
+        //   "success": function(data){
+        //     alert("Hai modificato la voce");
+        //
+        //   },
+        //   "error": function(){
+        //     alert();
+        //   }
+        // });
+
+
+  // $("#to-do-list").on("click", ".fa-times", function() {
+  //
+  // }
 
 
 
@@ -87,6 +121,20 @@ $(document).ready(function(){
 // -----------------------------
 // functions
 // -----------------------------
+function createList(){
+  $.ajax({
+    "url": "http://157.230.17.132:3007/todos",
+    "method": "GET",
+    "success": function(data){
+      for (var i = 0; i < data.length; i++) {
+        renderData(data[i]);
+      }
+    },
+    "error": function(){
+      alert();
+    }
+  });
+}
 
 function renderData(data){
   var source = document.getElementById("entry-template").innerHTML;
